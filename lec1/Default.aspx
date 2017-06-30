81f832ed0e3094e05d5034124107205146946a38
asp <asp@aptechnn.edu>
2017-06-30 18:34:26 +0500

    simplecluculationasp

---
+++ lec1/Default.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <br />
+        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="+" />
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click1" Text="-" />
+        <asp:Button ID="Button3" runat="server" OnClick="Button3_Click1" Text="*" />
+        <asp:Button ID="Button4" runat="server" OnClick="Button4_Click1" Text="/" />
+        <asp:Button ID="Button5" runat="server" OnClick="Button5_Click1" Text="sentific" />
+        <asp:Button ID="Button6" runat="server" OnClick="Button6_Click" Text="root" />
+        <asp:Button ID="Button7" runat="server" OnClick="Button7_Click" Text="log" />
+        <asp:Button ID="Button8" runat="server" OnClick="Button8_Click" Text="tan" />
+        <asp:Button ID="Button9" runat="server" OnClick="Button9_Click" Text="cos" />
+        <asp:Button ID="Button10" runat="server" OnClick="Button10_Click" Text="max" />
+        <br />
+        <br />
+        <br />
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        <br />
+        <br />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ lec1/Default.aspx.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    int a;
+    int b;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        a = Convert.ToInt32(TextBox1.Text);
+        b = Convert.ToInt32(TextBox2.Text);
+
+        int res = a + b;
+        Label1.Text = res.ToString();
+            
+    }
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button3_Click(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button4_Click(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button5_Click(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button2_Click1(object sender, EventArgs e)
+    {
+        a = Convert.ToInt32(TextBox1.Text);
+        b = Convert.ToInt32(TextBox2.Text);
+
+        int res = a - b;
+        Label1.Text = res.ToString();
+    }
+    protected void Button3_Click1(object sender, EventArgs e)
+    {
+        a = Convert.ToInt32(TextBox1.Text);
+        b = Convert.ToInt32(TextBox2.Text);
+
+        int res = a * b;
+        Label1.Text = res.ToString();
+    }
+    protected void Button4_Click1(object sender, EventArgs e)
+    {
+        a = Convert.ToInt32(TextBox1.Text);
+        b = Convert.ToInt32(TextBox2.Text);
+
+        int res = a / b;
+        Label1.Text = res.ToString();
+    }
+    protected void Button5_Click1(object sender, EventArgs e)
+    {
+        decimal res = Math.Round(Convert.ToDecimal(TextBox1.Text),Convert.ToInt32(TextBox2.Text));
+        Label1.Text = res.ToString();
+    }
+    protected void Button6_Click(object sender, EventArgs e)
+    {
+        double res = Math.Sqrt(Convert.ToDouble(TextBox1.Text));
+        Label1.Text = res.ToString();
+    }
+    protected void Button7_Click(object sender, EventArgs e)
+    {
+        double res = Math.Sign(Convert.ToDouble(TextBox1.Text));
+        Label1.Text = res.ToString();
+
+    }
+    protected void Button8_Click(object sender, EventArgs e)
+    {
+        double res = Math.Tan(Convert.ToDouble(TextBox1.Text));
+        Label1.Text = res.ToString();
+    }
+    protected void Button9_Click(object sender, EventArgs e)
+    {
+        double res = Math.Cos(Convert.ToDouble(TextBox1.Text));
+        Label1.Text = res.ToString();
+    }
+    protected void Button10_Click(object sender, EventArgs e)
+    {
+      decimal res = Math.Max(Convert.ToDecimal(TextBox1.Text), Convert.ToDecimal(TextBox2.Text));
+                Label1.Text = res.ToString();
+
+    }
+}
